9b562e09bdc4499553ad03da4bb9a3b48ba5b20a
keffri <keffri.w.neal@gmail.com>
2023-01-30 12:24:39 -0800

    created LandingPage component

---
+++ client/src/components/landingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const LandingPage: FC = () => {
+  return (
+    <div>
+      <h1>Hello landing page!</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
