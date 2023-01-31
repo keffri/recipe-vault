4ba1539607eb5ab386a70347a650ac028ecf01be
keffri <keffri.w.neal@gmail.com>
2023-01-31 10:16:53 -0800

    updated Navigation with bootstrap components

--- client/src/components/navigation/Navigation.tsx
+++ client/src/components/navigation/Navigation.tsx
@@ -1,10 +1,23 @@
 import React, { FC } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
 const Navigation: FC = () => {
   return (
-    <div>
-      <p>Navbar</p>
-    </div>
+    <Navbar expand="lg" className="navigation sticky-top">
+      <Container>
+        <Navbar.Brand>Recipe Vault</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="me_auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">Features</Nav.Link>
+            <Nav.Link href="#">Get Started!</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
