93158ddf9ba2c94a10e17a3eb4dc094789784a7d
keffri <keffri.w.neal@gmail.com>
2023-02-01 18:33:48 -0800

    updated LandingPage HTML

--- client/src/components/landingPage/LandingPage.tsx
+++ client/src/components/landingPage/LandingPage.tsx
@@ -1,10 +1,34 @@
 import React, { FC } from 'react';
+import LocalOfferOutlinedIcon from '@mui/icons-material/LocalOfferOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 
 const LandingPage: FC = () => {
   return (
-    <div>
-      <h1>Hello landing page!</h1>
-    </div>
+    <section className="landing">
+      <div className="landing__container">
+        <h1 className="landing__title">Recipe Bank</h1>
+        <div className="landing__description">
+          <ul className="landing__list">
+            <li>
+              <p>Create</p>
+              <AddCircleOutlineOutlinedIcon className="landing__icon" />
+            </li>
+            <li>
+              <p>Tag</p>
+              <LocalOfferOutlinedIcon className="landing__icon" />
+            </li>
+            <li>
+              <p>Save</p>
+              <BookmarkBorderOutlinedIcon className="landing__icon" />
+            </li>
+          </ul>
+        </div>
+        <p className="landing__text landing--medium">
+          Access all of your favorite recipes in one location!
+        </p>
+      </div>
+    </section>
   );
 };
 
