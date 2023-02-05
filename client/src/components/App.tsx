c67ac33007f0cb460f3768774ca3e8ad0145813c
keffri <keffri.w.neal@gmail.com>
2023-02-05 10:15:22 -0800

    added modal state

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,9 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './navigation/Navigation';
 import LandingPage from './landingPage/LandingPage';
 
 const App: FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <BrowserRouter>
       <div className="app">
