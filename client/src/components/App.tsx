707e5e58eaa865e42f3bc46ffc25780c535b9836
keffri <keffri.w.neal@gmail.com>
2023-02-05 10:40:50 -0800

    added Login + modal props

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,19 +1,25 @@
 import React, { FC, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './navigation/Navigation';
+import Login from './login/Login';
 import LandingPage from './landingPage/LandingPage';
 
 const App: FC = () => {
-  const [showModal, setShowModal] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
 
-  const modalDisplay = () => {
-    setShowModal(!showModal);
+  const openLoginModal = () => {
+    setShowLoginModal(true);
+  };
+
+  const closeLoginModal = () => {
+    setShowLoginModal(false);
   };
 
   return (
     <BrowserRouter>
       <div className="app">
         <Navigation modalDisplay={modalDisplay} />
+        {showModal && <Login />}
         <Routes>
           <Route
             path="/"
