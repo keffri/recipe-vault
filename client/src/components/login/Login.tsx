ff266f3cfa0da5de740660ac25be52906db044fb
keffri <keffri.w.neal@gmail.com>
2023-02-05 17:12:22 -0800

    class name additions

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -7,7 +7,7 @@ interface ModalProps {
 const Login: FC<ModalProps> = (props: ModalProps) => {
   return (
     <section className="login">
-      <h1>Login</h1>
+      <h1 className="login__title">Login</h1>
       <button className="login__close" onClick={() => props.closeLoginModal()}>
         Close
       </button>
