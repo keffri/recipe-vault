703ee99cceea8db232e161af67bff42dac8a9bcc
keffri <keffri.w.neal@gmail.com>
2023-02-06 10:34:08 -0800

    basic Login test

---
+++ client/src/__tests__/Login.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { findByTestId, render, screen } from '@testing-library/react';
+import Navigation, { ModalProps } from '../components/navigation/Navigation';
+
+function renderNavigation(props: Partial<ModalProps> = {}) {
+  const defaultProps: ModalProps = {
+    openLoginModal() {
+      return;
+    },
+  };
+  return render(<Navigation {...defaultProps} {...props} />);
+}
+
+test('Login modal displayed on button button click', async () => {
+  renderNavigation();
+
+  expect(screen.getByText('Recipe Vault')).toBeInTheDocument();
+});
