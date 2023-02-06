d259fc3a4d9e6adbbf7de489ce5d14725d63e076
keffri <keffri.w.neal@gmail.com>
2023-02-06 13:29:44 -0800

    Features component created

---
+++ client/src/components/features/Features.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Features: FC = () => {
+  return (
+    <section className="features">
+      <h1>Features</h1>
+    </section>
+  );
+};
+
+export default Features;
