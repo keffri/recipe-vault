5fe97cd0679cca3d96a4f7dc9177e9f8ac1c4ce9
keffri <keffri.w.neal@gmail.com>
2023-02-06 10:34:20 -0800

    updated Navigation test

--- client/src/__tests__/Navigation.test.tsx
+++ client/src/__tests__/Navigation.test.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Navigation from '../components/navigation/Navigation';
+import Navigation, { ModalProps } from '../components/navigation/Navigation';
+
+function renderNavigation(props: Partial<ModalProps> = {}) {
+  const defaultProps: ModalProps = {
+    openLoginModal() {
+      return;
+    },
+  };
+  return render(<Navigation {...defaultProps} {...props} />);
+}
 
 describe('Navigation rendering functionality', () => {
   it('Navigation text present', () => {
-    render(<Navigation />);
+    renderNavigation();
     const navigationText = screen.getByText('Recipe Vault');
     expect(navigationText).toBeInTheDocument();
   });
