134edb14992a33e28c9ca0f67aeb4e3a3811ef42
keffri <keffri.w.neal@gmail.com>
2023-02-07 11:54:52 -0800

    removed icon class

--- client/src/components/features/FeaturesCard.tsx
+++ client/src/components/features/FeaturesCard.tsx
@@ -13,7 +13,7 @@ interface CardProps {
 const FeaturesCard: FC<CardProps> = (props: CardProps) => {
   return (
     <section className="featuresCard">
-      <div className="featuresCard__icon">{props.card.icon}</div>
+      {props.card.icon}
       <h2 className="featuresCard__title">{props.card.title}</h2>
       <p className="featuresCard__description">{props.card.description}</p>
     </section>
