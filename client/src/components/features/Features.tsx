f77b8bf3c0a494d76035b8a5a993c49451f336ff
keffri <keffri.w.neal@gmail.com>
2023-02-07 10:53:35 -0800

    card type and card data added

--- client/src/components/features/Features.tsx
+++ client/src/components/features/Features.tsx
@@ -1,10 +1,39 @@
 import React, { FC } from 'react';
 import FeaturesCard from './FeaturesCard';
 
+type Card = {
+  title: string;
+  icon: any;
+  description: string;
+};
+
+const createFeature: Card = {
+  title: 'Create',
+  icon: 'icon',
+  description: 'description',
+};
+
+const tagFeature: Card = {
+  title: 'Tag',
+  icon: 'icon',
+  description: 'description',
+};
+
+const saveFeature: Card = {
+  title: 'Save',
+  icon: 'icon',
+  description: 'description',
+};
+
 const Features: FC = () => {
   return (
     <section className="features">
       <h1 className="features__title">Features</h1>
+      <div className="features__container">
+        <FeaturesCard card={createFeature} />
+        <FeaturesCard card={tagFeature} />
+        <FeaturesCard card={saveFeature} />
+      </div>
     </section>
   );
 };
