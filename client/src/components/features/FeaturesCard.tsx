78c92128f82350608ca545c2e771a91214a50523
keffri <keffri.w.neal@gmail.com>
2023-02-07 10:56:13 -0800

    type, props and basic structure

--- client/src/components/features/FeaturesCard.tsx
+++ client/src/components/features/FeaturesCard.tsx
@@ -1,9 +1,21 @@
 import React, { FC } from 'react';
 
-const FeaturesCard: FC = () => {
+type Card = {
+  title: string;
+  icon: any;
+  description: string;
+};
+
+interface CardProps {
+  card: Card;
+}
+
+const FeaturesCard: FC<CardProps> = (props: CardProps) => {
   return (
     <section className="featuresCard">
-      <h1>Features Card</h1>
+      <h1>{props.card.title}</h1>
+      {props.card.icon}
+      <p>{props.card.description}</p>
     </section>
   );
 };
