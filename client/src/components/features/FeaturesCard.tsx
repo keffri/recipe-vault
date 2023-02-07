578e8d52a16eebb00e8b2f072ec47c65488fbe58
keffri <keffri.w.neal@gmail.com>
2023-02-07 10:24:21 -0800

    created FeaturesCard component

---
+++ client/src/components/features/FeaturesCard.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const FeaturesCard: FC = () => {
+  return (
+    <section className="featuresCard">
+      <h1>Features Card</h1>
+    </section>
+  );
+};
+
+export default FeaturesCard;
