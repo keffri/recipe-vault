a88606f707afc3e39ccba72bb1a1567ff246b929
keffri <keffri.w.neal@gmail.com>
2023-02-09 13:40:17 -0800

    login button functionality

--- client/src/components/login/LoginButton.tsx
+++ client/src/components/login/LoginButton.tsx
@@ -1,7 +1,33 @@
 import React, { FC } from 'react';
+import { Google } from '@mui/icons-material';
+import { useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 const LoginButton: FC = () => {
-  return <button>Login</button>;
+  const login = useGoogleLogin({
+    onSuccess: async (response) => {
+      try {
+        const data = await axios.get(
+          'https://www.googleapis.com/oauth2/v3/userinfo',
+          {
+            headers: {
+              Authorization: `Bearer ${response.access_token}`,
+            },
+          }
+        );
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
+
+  return (
+    <button className="loginButton" onClick={() => login()}>
+      <p className="loginButton__text">Continue with Google</p>
+      <Google />
+    </button>
+  );
 };
 
 export default LoginButton;
