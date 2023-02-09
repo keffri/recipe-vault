ff2791b138ec0a7629a846d0f41b22e305f4381c
keffri <keffri.w.neal@gmail.com>
2023-02-09 14:27:02 -0800

    simple user welcome message

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,9 +1,20 @@
 import React, { FC } from 'react';
 
-const Home: FC = () => {
+type User = {
+  email: string;
+  name: string;
+  picture: string;
+};
+
+interface UserProps {
+  user: User | null;
+}
+
+const Home: FC<UserProps> = (props: UserProps) => {
   return (
     <section className="home">
       <h1 className="home__title">Home</h1>
+      <p>Welcome {props.user!.name}!</p>
     </section>
   );
 };
