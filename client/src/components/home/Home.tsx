b1afe0a7ecd44858afc5b5a1724051c352fe2a54
keffri <keffri.w.neal@gmail.com>
2023-02-09 14:23:02 -0800

    created home component

---
+++ client/src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Home: FC = () => {
+  return (
+    <section className="home">
+      <h1 className="home__title">Home</h1>
+    </section>
+  );
+};
+
+export default Home;
