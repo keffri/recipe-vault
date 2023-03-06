77ca900e3a0ee6ad4e2ef97aee32146706391d03
keffri <keffri.w.neal@gmail.com>
2023-03-06 12:59:32 -0800

    signup post route enabled

---
+++ server/api/routes.ts
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const auth_controller = require('../src/controllers/authController');
+
+// AUTH
+router.post('/signup', auth_controller.signup_post);
+// router.post("/login", auth_controller.login_post)
