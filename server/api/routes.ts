de1c7273e6a21b3daff3ee8372aa454cd1965c82
keffri <keffri.w.neal@gmail.com>
2023-03-07 10:31:27 -0800

    enabled login post

--- server/api/routes.ts
+++ server/api/routes.ts
@@ -5,6 +5,6 @@ const auth_controller = require('../src/controllers/authController');
 
 // AUTH
 router.post('/signup', auth_controller.signup_post);
-// router.post("/login", auth_controller.login_post)
+router.post('/login', auth_controller.login_post);
 
 module.exports = router;
