9cb7045146d89be8f7f11d62b01ade0486a8557b
keffri <keffri.w.neal@gmail.com>
2023-03-16 13:37:58 -0700

    added recipe type

--- client/src/components/create/Create.tsx
+++ client/src/components/create/Create.tsx
@@ -1,5 +1,21 @@
 import React, { FC } from 'react';
 
+type Recipe = {
+  name: string;
+  course: string;
+  cuisine: string;
+  diet: string;
+  tags: string[];
+  prep_time: number;
+  cook_time: number;
+  total_time: number;
+  serves: number;
+  ingredients: string[];
+  instructions: string[];
+  notes: string[];
+  link: string;
+};
+
 const Create: FC = () => {
   return (
     <section className="create">
