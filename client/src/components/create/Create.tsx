67830b4d76c1c66a68d20b3e5e776cfd2bba8752
keffri <keffri.w.neal@gmail.com>
2023-03-23 10:28:58 -0700

    added class names

--- client/src/components/create/Create.tsx
+++ client/src/components/create/Create.tsx
@@ -22,30 +22,66 @@ const Create: FC = () => {
       <h1 className="create__title">Create</h1>
 
       <form className="create__form">
-        <label htmlFor="name">Name:</label>
-        <input type="text" placeholder="recipe name..." />
-        <label htmlFor="cuisine">Cuisine</label>
-        <input type="text" placeholder="cuisine..." />
-        <label htmlFor="course">Course:</label>
-        <input type="text" placeholder="course..." />
-        <label htmlFor="tags">Tags:</label>
-        <input type="text" placeholder="tag..." />
-        <label htmlFor="prep_time">Prep time:</label>
-        <input type="number" />
-        <label htmlFor="cook_time">Cook time:</label>
-        <input type="number" />
-        <label htmlFor="total_time">Total time:</label>
-        <input type="number" />
-        <label htmlFor="serves">Serves:</label>
-        <input type="number" />
-        <label htmlFor="ingredients">Ingredients:</label>
-        <input type="text" placeholder="ingredient..." />
-        <label htmlFor="instructions">Instructions:</label>
-        <input type="text" placeholder="instruction..." />
-        <label htmlFor="notes">Notes:</label>
-        <input type="text" placeholder="note..." />
-        <label htmlFor="link">Link</label>
-        <input type="text" placeholder="link..." />
+        <label className="create__label" htmlFor="name">
+          Name:
+        </label>
+        <input
+          className="create__input"
+          type="text"
+          placeholder="recipe name..."
+        />
+        <label className="create__label" htmlFor="cuisine">
+          Cuisine
+        </label>
+        <input className="create__input" type="text" placeholder="cuisine..." />
+        <label className="create__label" htmlFor="course">
+          Course:
+        </label>
+        <input className="create__input" type="text" placeholder="course..." />
+        <label className="create__label" htmlFor="tags">
+          Tags:
+        </label>
+        <input className="create__input" type="text" placeholder="tag..." />
+        <label className="create__label" htmlFor="prep_time">
+          Prep time:
+        </label>
+        <input className="create__input" type="number" />
+        <label className="create__label" htmlFor="cook_time">
+          Cook time:
+        </label>
+        <input className="create__input" type="number" />
+        <label className="create__label" htmlFor="total_time">
+          Total time:
+        </label>
+        <input className="create__input" type="number" />
+        <label className="create__label" htmlFor="serves">
+          Serves:
+        </label>
+        <input className="create__input" type="number" />
+        <label className="create__label" htmlFor="ingredients">
+          Ingredients:
+        </label>
+        <input
+          className="create__input"
+          type="text"
+          placeholder="ingredient..."
+        />
+        <label className="create__label" htmlFor="instructions">
+          Instructions:
+        </label>
+        <input
+          className="create__input"
+          type="text"
+          placeholder="instruction..."
+        />
+        <label className="create__label" htmlFor="notes">
+          Notes:
+        </label>
+        <input className="create__input" type="text" placeholder="note..." />
+        <label className="create__label" htmlFor="link">
+          Link
+        </label>
+        <input className="create__input" type="text" placeholder="link..." />
       </form>
     </section>
   );
