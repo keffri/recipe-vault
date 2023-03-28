e304bd800676cdfa2f696f5ecc530c13cfadf78f
keffri <keffri.w.neal@gmail.com>
2023-03-27 18:08:44 -0700

    user access fail without AuthToken

--- client/src/components/vault/Vault.tsx
+++ client/src/components/vault/Vault.tsx
@@ -1,9 +1,23 @@
 import React, { FC } from 'react';
 
-const Vault: FC = () => {
+interface VaultProps {
+  cookies: { [x: string]: any };
+}
+
+const Vault: FC<VaultProps> = (props: VaultProps) => {
   return (
     <section className="vault">
-      <h1 className="vault__title">Vault</h1>
+      {!props.cookies.AuthToken && (
+        <div className="vault__fail">
+          <h2>Please log in if you wish to view your recipes.</h2>
+        </div>
+      )}
+
+      {props.cookies.AuthToken && (
+        <div className="vault__pass">
+          <h1 className="vault__title">Vault</h1>
+        </div>
+      )}
     </section>
   );
 };
