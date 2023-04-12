639cb5af15a1203f4b663dc5aa6949ec7ddd812a
keffri <keffri.w.neal@gmail.com>
2023-04-11 17:39:16 -0700

    imported create_controller

--- server/api/routes.ts
+++ server/api/routes.ts
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const auth_controller = require('../src/controllers/authController');
+const create_controller = require('../src/controllers/createController');
 
 // AUTH
 router.post('/signup', auth_controller.signup_post);
