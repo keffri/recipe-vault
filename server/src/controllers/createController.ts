7ec10a5b20acee764d5829213b495feae4d209dd
keffri <keffri.w.neal@gmail.com>
2023-04-11 17:39:28 -0700

    create_post method created

--- server/src/controllers/createController.ts
+++ server/src/controllers/createController.ts
@@ -2,3 +2,5 @@ const pool = require('../db');
 const { v4: uuidv4 } = require('uuid');
 const { body, validationResult } = require('express-validator');
 import { Request, Response } from 'express';
+
+exports.create_post = async (req: Request, res: Response) => {};
