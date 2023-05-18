fe3ff9f4e6f48b1f5e3595a98a8ca3776e69268e
keffri <keffri.w.neal@gmail.com>
2023-05-18 16:10:43 -0700

    create controller express-validator additions

--- server/src/controllers/createController.ts
+++ server/src/controllers/createController.ts
@@ -3,4 +3,47 @@ const { v4: uuidv4 } = require('uuid');
 const { body, validationResult } = require('express-validator');
 import { Request, Response } from 'express';
 
-exports.create_post = async (req: Request, res: Response) => {};
+exports.create_post = [
+  body('name')
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage('You must enter a longer recipe name.')
+    .isLength({ max: 30 })
+    .escape()
+    .withMessage('You must enter a shorter recipe name'),
+  body('cuisine')
+    .trim()
+    .isLength({ min: 4 })
+    .escape()
+    .withMessage('You must enter at least one cuisine.'),
+  body('course')
+    .trim()
+    .isLength({ min: 4 })
+    .escape()
+    .withMessage('You must enter at least one course.'),
+  body('tag')
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage('You must enter at least one tag.'),
+  body('ingredients')
+    .trim()
+    .isLength({ min: 4 })
+    .escape()
+    .withMessage('Recipe must contain at least one ingredient.'),
+  body('instructions')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Recipe must contain at least one instruction.'),
+
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({ errors: errors.array() });
+      console.log(errors);
+      return;
+    }
+  },
+];
